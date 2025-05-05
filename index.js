327e1b17e82078c7199ee3eef2c06fc1a5fce8b7
Irfan Khan <khanuiux86@gmail.com>
2025-05-05 13:05:22 +0530

    functionality added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let badgeSectionEl = document.querySelector('.badgeSection');
 let AboutUsLeftEl = document.querySelector('.leftDesign');
 
+
 const generateMiniElement = () => {
     const topDot = document.createElement('div');
     topDot.classList.add('topDot');
@@ -18,3 +19,30 @@ const generateMiniElement = () => {
 }
 
 generateMiniElement()
+
+const myArray = ["UI UX Designer", "Front-end Developer", "Web Developer", "Web Designer"];
+const changeMeEl = document.querySelector('.changeMe');
+
+let letterIndex = 0;
+let wordIndex = 0;
+let currentWord = "";
+let intervalID;
+
+function typeWord() {
+    if (letterIndex < myArray[wordIndex].length) {
+        currentWord += myArray[wordIndex][letterIndex];
+        changeMeEl.textContent = currentWord; 
+        letterIndex++; 
+    } else {
+        clearInterval(intervalID);
+        
+        setTimeout(() => {
+            letterIndex = 0;
+            currentWord = "";
+            wordIndex = (wordIndex + 1) % myArray.length;
+            intervalID = setInterval(typeWord, 200);
+        }, 1000);
+    }
+}
+
+intervalID = setInterval(typeWord, 150); 
